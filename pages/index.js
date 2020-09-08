0d9d052e515d2d07cfb9bb1c41231c629a88e706
AldoMJ <68668465+AldoMJ@users.noreply.github.com>
2020-09-08 19:57:52 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 export default function Index() {
   const { data, error } = useSWR('/api/people', fetcher)
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>Falha na Leitura</div>
+  if (!data) return <div>Carregando...</div>
 
   return (
     <ul>
       {data.map((p, i) => (
-        <Person key={i} person={p} />
+       <Person key={i} person={p} />
       ))}
     </ul>
   )
