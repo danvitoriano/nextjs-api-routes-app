56e2f88bbe7222967367d39b4d9811d10fb5ed90
teixeira308 <teixeira308@Hotmail.com>
2020-09-14 18:08:00 -0300

    pagina apresentation

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
 import useSWR from 'swr'
 import Person from '../components/Person'
+import Link from 'next/link'
+
+
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
   const { data, error } = useSWR('/api/people', fetcher)
+  const { dataApresentation, errorApresentation } = useSWR('/apresentation', fetcher)
 
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
-  return (
+  return (<>
+      <Link href="/apresentation/">
+          <a>Apresentation</a>
+        </Link>
     <ul>
       {data.map((p, i) => (
         <Person key={i} person={p} />
       ))}
     </ul>
+
+   </>
+  
   )
 }
